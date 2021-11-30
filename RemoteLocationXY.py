47533a3ebfe8b6e70511e4131a3ea44417aa7d7b
Dorn Moore <dorn@savingcranes.org>
2021-11-30 14:51:18 -0600

    Update RemoteLocationXY.py

--- RemoteLocationXY.py
+++ RemoteLocationXY.py
@@ -162,7 +162,7 @@ arcpy.mapping.AddLayer(df, newlayer0,"TOP")
 arcpy.mapping.AddLayer(df, newlayer1,"TOP")
 
 ## Get relative path of the executing script. It is required for creating path to the templates (*.lyr-files) of symbology   
-relatpath = os.path.dirname(__file__) # in case of error here you can use alternative way: os.path.realpath(os.path.dirname(sys.argv[0]))
+relatpath = os.path.realpath(os.path.dirname(sys.argv[0]))
 
 ## Set symbology of the buffer according to the "startpoints.lyr" template
 try:
